6e499d2956d0cad2f28958b4525bc52276dcc2af
rajabilal0 <109717702+rajabilal0@users.noreply.github.com>
2022-10-21 19:05:25 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['51 985 752 203'] //ur owner number
+global.vcardowner = ['923150623466'] //ur owner number
 global.ownername = "nipuna rangana" //ur owner name
 global.ytname = "YT: 𝐂𝐈𝐁𝐄𝐑 𝐓𝐄𝐂𝐇" //ur yt chanel name
 global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy
-global.owner = ['51 985 752 203'] //ur number 
-global.ownertag = '51 985 752 203' //ur tag number
-global.botname = '📶 SAJJAD KING 📶' //ur bot name
-global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/3J9-8U8St3U" //ur website to be displayed
+global.owner = ['923150623466] //ur number 
+global.ownertag = '923150623466' //ur tag number
+global.botname = '📶 BILAL KING 📶' //ur bot name
+global.linkz = "https://youtube.com/channel/UCuoNu27J8WUagCfD9lSzC0g" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCuoNu27J8WUagCfD9lSzC0g" //ur website to be displayed
 global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "📶 SAJJAD KING  📶" //ur sticker watermark packname
-global.author = "📶 SAJJAD KING 📶" //ur sticker watermark author
+global.packname = "📶 BILAL KING  📶" //ur sticker watermark packname
+global.author = "📶 BILAL KING 📶" //ur sticker watermark author
 global.menumoji ="💖"
 global.menuimg = "https://i.ibb.co/0mDLtyr/f2337149532299-58b7611d91faf.jpg" //ur alive img link [https://i.ibb.co]
 global.alive = "https://raw.githubusercontent.com/Nipuna-apps/Blue-Lion-Bot-V2/main/LioneMedia/video/Lione-bot.mp4" //ur alive video or image link [github.com]
@@ -81,7 +81,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['94714188730'] //ur premium numbers
+global.premium = ['923150623466'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
